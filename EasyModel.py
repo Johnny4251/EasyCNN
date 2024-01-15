c07cd652adc50d6bd937953c51a714c9fc7be44c
= <=>
2024-01-14 21:24:39 -0500

    Introducing VGGNet

--- EasyModel.py
+++ EasyModel.py
@@ -26,6 +26,19 @@ class EasyCNN:
         v2.ConvertImageDtype()
     ])
 
+    vgg_train_transform = v2.Compose([
+        v2.Resize((224, 224)),
+        v2.RandomHorizontalFlip(),
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    vgg_test_transform = v2.Compose([
+        v2.Resize((224, 224)),
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
     def __init__(self, model, train_transform=simple_transform, grayscale=False):
         self.train_transform = train_transform
         self.test_transform = self.simple_transform
@@ -33,6 +46,7 @@ class EasyCNN:
         self.grayscale = grayscale
 
         self.device = ("cuda" if torch.cuda.is_available() else "cpu")
+        print("Using ", self.device)
 
     def __train_one_epoch(self, train_dataloader, loss_fn, optimizer):
         size = len(train_dataloader.dataset)
@@ -235,3 +249,95 @@ class ExampleModelV2(nn.Module):
     def forward(self, x):
         logits = self.net_stack(x)
         return logits
+    
+# https://blog.paperspace.com/vgg-from-scratch-pytorch/
+class VGG16(nn.Module):
+    def __init__(self, num_classes=10):
+        super(VGG16, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(), 
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU())
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer5 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer6 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU())
+        self.layer7 = nn.Sequential(
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer8 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer9 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer10 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.layer11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU())
+        self.layer13 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride = 2))
+        self.fc = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(7*7*512, 4096),
+            nn.ReLU())
+        self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU())
+        self.fc2= nn.Sequential(
+            nn.Linear(4096, num_classes))
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = self.layer12(out)
+        out = self.layer13(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+        return out
--- example.py
+++ example.py
@@ -1,11 +1,15 @@
 from EasyModel import EasyCNN
 from EasyModel import ExampleModelV2 as EM
+from EasyModel import VGG16
 
 # Color image(128x128) w/ class count of 3
 model = EM(3, 3, 128)
+vgg_model = VGG16(5)
 
 # Using the default train_transform in EasyCNN
-easycnn = EasyCNN(model, train_transform=EasyCNN.example_train_transform)
-easycnn.train_model(1, 32, save_model=True, model_name="ExampleModel")
+#easycnn = EasyCNN(model, train_transform=EasyCNN.example_train_transform)
+#easycnn.train_model(1, 32, save_model=True, model_name="ExampleModel")
 
-
+# Training a model using built-in vgg16 model
+vgg_trainer = EasyCNN(vgg_model, EasyCNN.vgg_train_transform, grayscale=False)
+vgg_trainer.train_model(total_epochs=1, batch_size=64, save_model=True, model_name="CustomVGGModel")
